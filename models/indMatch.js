714ce8ab4571d2639ba317cf8ee159e0933dc290
luosuiqian <luosuiqian@126.com>
2014-08-19 22:36:25 +0800

    Refactor the space

--- models/indMatch.js
+++ models/indMatch.js
@@ -29,7 +29,7 @@ create table if not exists indUser
 var util = require('util');
 var conn = require('./db').getConnection;
 
-function getPosition(total, leftP, rightP) {
+var getPosition = function (total, leftP, rightP) {
   var dis = rightP - leftP + 1;
   if (leftP > rightP) {
     dis = leftP - rightP + 1;
@@ -44,10 +44,11 @@ function getPosition(total, leftP, rightP) {
     row = parseInt((leftP + rightP) / 2 + 1);
   }
   return {'row':row, 'col':col};
-}
+};
 
 exports.get = function (year, type, callback) {
-  conn().query('SELECT leftP, rightP, id1, id2, id3, id4, score12, score34, detail FROM indMatch\
+  conn().query('SELECT leftP, rightP, id1, id2, id3, id4, \
+                score12, score34, detail FROM indMatch \
                 WHERE year = ? and type = ?',
                [year, type], function(err, indMatch) {
     if (err) {
@@ -59,55 +60,61 @@ exports.get = function (year, type, callback) {
         return callback(err, null);
       }
       var name = {};
-      for (var i=0;i<indUser.length;i++) {
+      for (var i = 0; i < indUser.length; i++) {
         name[indUser[i].id] = indUser[i].name;
       }
       var total = 0;
-      for (var i=0;i<indMatch.length;i++) {
+      for (var i = 0; i < indMatch.length; i++) {
         if (total < indMatch[i].rightP) total = indMatch[i].rightP;
       }
       var pos = getPosition(total, 1, total);
       var table = new Array(total + 1);
-      for (var i=0;i<table.length;i++) {
+      for (var i = 0; i < table.length; i++) {
         table[i] = new Array(pos.col + 1);
-        for (var j=0;j<table[i].length;j++) {
+        for (var j = 0; j < table[i].length; j++) {
           table[i][j] = {};
           table[i][j].height = 0;
           table[i][j].content = ['（空）','',''];
           table[i][j].detail = '';
         }
       }
-      for (var i=1;i<table[0].length;i++) {
+      for (var i = 1; i < table[0].length; i++) {
         if (i == table[0].length - 1) table[0][i].content = '决赛/三四名';
         else if (i == table[0].length - 2) table[0][i].content = '半决赛';
         else table[0][i].content = '第' + i + '轮';
       }
-      for (var i=1;i<table.length;i++) {
+      for (var i = 1; i < table.length; i++) {
         table[i][0].height = 1;
         if (type == 1) {
           table[i][0].content = name[i];
         } else {
-          table[i][0].content = util.format("%s/%s", name[i*2-1], name[i*2]);
+          table[i][0].content = util.format("%s/%s", name[i * 2 - 1], name[i * 2]);
         }
       }
       var height = 1;
-      for (var c=1; c<table[0].length; c++) {
+      for (var c = 1; c < table[0].length; c++) {
         if (c != table[0].length - 1) {
           height = height * 2;
         }
-        for (var i=1;i<table.length;i+=height) {
+        for (var i = 1; i < table.length; i += height) {
           table[i][c].height = height;
         }
       }
-      for (var i=0;i<indMatch.length;i++) {
+      for (var i = 0; i < indMatch.length; i++) {
         var pos = getPosition(total, indMatch[i].leftP, indMatch[i].rightP);
         var str;
         if (type == 1) {
-          str = util.format("%s\t%d-%d\t%s", name[indMatch[i].id1], indMatch[i].score12,
-                            indMatch[i].score34, name[indMatch[i].id3]);
+          str = util.format("%s\t%d-%d\t%s",
+                            name[indMatch[i].id1],
+                            indMatch[i].score12,
+                            indMatch[i].score34,
+                            name[indMatch[i].id3]);
         } else {
-          str = util.format("%s/%s\t%d-%d\t%s/%s", name[indMatch[i].id1], name[indMatch[i].id2], indMatch[i].score12,
-                            indMatch[i].score34, name[indMatch[i].id3], name[indMatch[i].id4]);
+          str = util.format("%s/%s\t%d-%d\t%s/%s",
+                            name[indMatch[i].id1], name[indMatch[i].id2],
+                            indMatch[i].score12,
+                            indMatch[i].score34,
+                            name[indMatch[i].id3], name[indMatch[i].id4]);
         }
         table[pos.row][pos.col].content = str.split('\t');
         table[pos.row][pos.col].detail = indMatch[i].detail;
@@ -119,12 +126,12 @@ exports.get = function (year, type, callback) {
 
 exports.newMatch = function (year, type, round) {
   var total = 1;
-  for (var i=1;i<=round;i++) total*=2;
+  for (var i = 1; i <= round; i++) total *= 2;
   var height = 1;
   var id = 0;
-  for (var c=1; c<=round; c++) {
+  for (var c = 1; c <= round; c++) {
     height = height * 2;
-    for (var i=1;i<=total;i+=height) {
+    for (var i = 1; i <= total; i += height) {
       var line = {};
       line.year = year;
       line.type = type;
@@ -158,7 +165,7 @@ exports.newMatch = function (year, type, round) {
 };
 
 exports.newUser = function (year, type, begin, end) {
-  for (var c=begin; c<=end; c++) {
+  for (var c = begin; c <= end; c++) {
     var line = {};
     line.year = year;
     line.type = type;
